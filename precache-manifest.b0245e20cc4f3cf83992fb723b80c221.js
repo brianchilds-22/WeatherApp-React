7aa8305c1ce04dabb5c13734b9a4ecf2f450b801
brianchilds-22 <brianchilds22@msn.com>
2021-03-29 10:26:37 -0600

    Updates

---
+++ precache-manifest.b0245e20cc4f3cf83992fb723b80c221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1bdeb84585ec4f2df0",
+    "url": "/WeatherApp-React/static/js/main.b758fe27.chunk.js"
+  },
+  {
+    "revision": "daa9d939791c6e9e7f5f",
+    "url": "/WeatherApp-React/static/js/runtime~main.861b80c9.js"
+  },
+  {
+    "revision": "be8d853ba912082bcdcf",
+    "url": "/WeatherApp-React/static/css/2.04fa3980.chunk.css"
+  },
+  {
+    "revision": "be8d853ba912082bcdcf",
+    "url": "/WeatherApp-React/static/js/2.35232a28.chunk.js"
+  },
+  {
+    "revision": "09e99aa86089d4c474f449eec97bf75e",
+    "url": "/WeatherApp-React/index.html"
+  }
+];
